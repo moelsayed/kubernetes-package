f71a76d3cd18a2d59569018dc9d82ffa3ff605d7
moelsayed <m.elsayed@gmail.com>
2018-07-18 22:20:43 +0200

    Use bind-mount fix for cgroups issue

--- package/entry.sh
+++ package/entry.sh
@@ -84,17 +84,19 @@ if [ "$1" == "kubelet" ]; then
     done
     mount --rbind /host/dev /dev
     mount -o rw,remount /sys/fs/cgroup 2>/dev/null || true
-    if ! [ /sys/fs/cgroup/cpu,cpuacct -ef /sys/fs/cgroup/cpuacct,cpu ]
-    then 
-      rm -rf  /sys/fs/cgroup/cpuacct,cpu
-      ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu 2>/dev/null || true
-    fi
+    
     for i in /sys/fs/cgroup/*; do
         if [ -d $i ]; then
              mkdir -p $i/kubepods
         fi
     done
 fi
+
+mkdir -p /sys/fs/cgroup/cpuacct,cpu/
+mount --bind /sys/fs/cgroup/cpu,cpuacct/ /sys/fs/cgroup/cpuacct,cpu/
+mkdir -p /sys/fs/cgroup/net_prio,net_cls/
+mount --bind /sys/fs/cgroup/net_cls,net_prio/ /sys/fs/cgroup/net_prio,net_cls/
+
 
 FQDN=$(hostname --fqdn || hostname)
 
